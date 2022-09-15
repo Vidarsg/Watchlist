239be6ed3c8ae7a300460f4c3205ad4b1f07d866
Viktor Tingstad <viktorti@stud.ntnu.no>
2022-09-15 13:35:10 +0200

    Basic functionality to controller and FXML

--- watchlist-project/src/main/java/watchlist/WatchlistController.java
+++ watchlist-project/src/main/java/watchlist/WatchlistController.java
@@ -1,5 +1,17 @@
 package watchlist;
 
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class WatchlistController {
-    
+    @FXML
+    private ListView<String> moviebrowser;
+
+    public void initialize() {
+        List<String> list = List.of("a, 2022","b, 2019");
+        moviebrowser.setItems(FXCollections.observableList(list));
+    }
 }
