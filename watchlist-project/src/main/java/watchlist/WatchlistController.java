f17494ad656f6aee503f25d6818e79c7cedb1173
Viktor Tingstad <viktorti@stud.ntnu.no>
2022-09-15 14:01:26 +0200

    Started on listview of movies in controller Issue #2

--- watchlist-project/src/main/java/watchlist/WatchlistController.java
+++ watchlist-project/src/main/java/watchlist/WatchlistController.java
@@ -1,6 +1,8 @@
 package watchlist;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -11,7 +13,11 @@ public class WatchlistController {
     private ListView<String> moviebrowser;
 
     public void initialize() {
-        List<String> list = List.of("a, 2022","b, 2019");
-        moviebrowser.setItems(FXCollections.observableList(list));
+        updateMovies();
+    }
+
+    public void updateMovies() {
+        ArrayList<Movie> list = new ArrayList<Movie>(Arrays.asList(new Movie("Movie A", 2020), new Movie("Movie B", 2021), new Movie("Movie C", 2022)));
+        moviebrowser.setItems(FXCollections.observableArrayList(list.stream().map(x -> x.toString()).collect(Collectors.toList())));
     }
 }
