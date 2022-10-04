aed92b30812994f2e1737c00f9be58335bf84720
vidarsgl <vidarsgl@stud.ntnu.no>
2022-10-04 11:06:05 +0200

    Removed imports
    Deletion of unnecessary javafx-imports.

--- watchlist-project/src/main/java/watchlist/WatchlistApp.java
+++ watchlist-project/src/main/java/watchlist/WatchlistApp.java
@@ -2,12 +2,9 @@ package watchlist;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
