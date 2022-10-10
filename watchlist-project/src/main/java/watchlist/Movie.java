3d9c4a9e67c756bcbd2f4a8a2a1cb36d867f0a13
Viktor Tingstad <viktorti@stud.ntnu.no>
2022-10-10 20:29:46 +0200

    Bug fixes to movie object Bug fixes to prevent major delay when creating a lot of movie-objects at the same time.
    Issue: #2

--- watchlist-project/src/main/java/watchlist/Movie.java
+++ watchlist-project/src/main/java/watchlist/Movie.java
@@ -3,7 +3,6 @@ package watchlist;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.scene.image.Image;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -19,8 +18,6 @@ public class Movie {
 
     private String image_url;
     private String thumb_url;
-    private Image image;
-    private Image thumb;
 
     /**
      * Creates a new Movie object with the given title and year
@@ -45,25 +42,14 @@ public class Movie {
         this.directors = directors;
         this.genre = genre;
 
-        try {
-            this.image = new Image(image_url);
-            this.image_url = image_url;
-            this.thumb = new Image(thumb_url);
-            this.thumb_url = thumb_url;
-        } catch (Exception e) {
-            String url = "https://www.popcorn.app/assets/app/images/placeholder-movieimage.png";
-            this.image = new Image(url);
-            this.image_url = url;
-            this.thumb = new Image(url);
-            this.thumb_url = url;
-        }
+        this.image_url = image_url;
+        this.thumb_url = thumb_url;
     }
     public Movie(String name, int year, String desc, double rating) {
         new Movie(name, year, desc, rating, null, null, null, null, null);
     }
     // Temporary to prevent errors
     public Movie(String name, int year) {
-        //new Movie(name, year, "desc", 1.0, null, null, null, null, null);
         this.name = name;
         this.year = year;
     }
@@ -79,9 +65,7 @@ public class Movie {
     public List<String> getGenre() {return new ArrayList<String>(genre);}
 
     public String getImage_url() {return image_url;}
-    public Image getImage() {return image;}
     public String getThumb_url() {return thumb_url;}
-    public Image getThumb() {return thumb;}
     // ! Getters
 
     // Methods
