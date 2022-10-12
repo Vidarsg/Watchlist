7ef9bd7b5706de058e879924d8b2288ac4ccac2e
vidarsgl <vidarsgl@stud.ntnu.no>
2022-10-12 15:45:45 +0200

    Style changes
    Added some style changes that weren't included in previous merge.
    
    Issue: #12

--- watchlist-project/fxui/src/main/java/watchlist/ui/LoginController.java
+++ watchlist-project/fxui/src/main/java/watchlist/ui/LoginController.java
@@ -47,7 +47,7 @@ public class LoginController {
 			        stage.setTitle("Watchlist");
 			        stage.setResizable(false);
 			        stage.setScene(new Scene(root));
-                    //stage.getIcons().add(new Image("file:fxui/src/main/resources/watchlist/ui/images/watchlist-favicon.png"));
+                    stage.getIcons().add(new Image("file:fxui/src/main/resources/watchlist/ui/images/watchlist-favicon.png"));
 			        stage.show();
 			        login.getScene().getWindow().hide();
 			        
